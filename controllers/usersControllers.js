cd4b5b85c94d0f53d6e7f79a194d18950f84e4ef
Nirupa <nirupasinha4@gmail.com>
2020-12-24 09:09:39 +0530

    axios third party call

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -5,17 +5,19 @@ const handler = require("../responseHandler")
 module.exports = {
     details: (req, res) => {
         const userObject = req.body;
+        console.log("=============", userObject.email);
         db.getUserDetails(User, userObject).then(function successResponse(dbData) {
             if (!dbData) {
                 let message = `User does not exist`;
-                return handler.responseHandler(res, 500, message, dbData)
+                return handler.responseHandler(res, 400, message, null, dbData)
             } else {
+                console.log("get data", dbData);
                 let message = `User Details find Successfully`;
-                return handler.responseHandler(res, 500, message, dbData)
+                return handler.responseHandler(res, 200, message, null, dbData)
             }
         }).catch(function errorResponse(err) {
             let message = `e`;
-            return handler.responseHandler(res, 500, err, message)
+            return handler.responseHandler(res, 500, message, err)
         })
     },
     updateProfile: (req, res) => {
